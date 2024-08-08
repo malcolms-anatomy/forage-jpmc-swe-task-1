966aabb8333dd5ccd2aede911a1214305cdfc642
malcolms-anatomy <malcolmihere@outlook.com>
2024-08-08 01:05:54 +0100

    Fixed price computation and ratio calculation in client3.py

--- client3.py
+++ client3.py
@@ -31,18 +31,21 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ ------------- Updated this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2   # The price is now updated to be computed as the average of bid_price and ask_price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """ ------------- Updated this function ------------- """
+    # Return the ratio of price_a to price_b
+    if price_b == 0:
+        return
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +54,13 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        """ ----------- Updated to get the correct ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Ensure both stocks are present before calculating the ratio
+        if "ABC" in prices and "DEF" in prices:
+            print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
